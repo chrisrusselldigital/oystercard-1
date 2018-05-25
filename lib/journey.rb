cf4d752361501bbbf661dd2bdc37e3b51bc72d71
Rahul Rakshit <rahulrakshit@gmx.com>
2018-05-25 15:51:19 +0100

    test-drive basic methods of Journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,10 +1,13 @@
 class Journey
+  MINIMUM_CHARGE = 3
+  attr_reader :entry_station, :exit_station
 
+  def complete?
+    true
+  end
 
+  def fare
+    MINIMUM_CHARGE
+  end
 
 end
-
-
-journey = Journey.new("toss")
-
-p journey
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,15 +3,20 @@ require 'journey'
 
 describe Journey do
 
-context '#initialize' do
-  it "initialized with 1 argument" do
-    expect(Journey).to respond_to(:new).with(1).argument
+  it "#entry_station" do
+    expect(subject).to respond_to(:entry_station)
   end
 
+  it "#exit_station" do
+    expect(subject).to respond_to(:exit_station)
+  end
 
-end
+  it "#complete?" do
+    expect(subject.complete?).to be(true).or be(false)
+  end
 
-
-
+  it "#fare" do
+    expect(subject.fare).to eq Journey::MINIMUM_CHARGE
+  end
 
 end
